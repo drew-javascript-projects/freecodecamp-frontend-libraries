0f1b6272e7db507d33798d826fb5a1b26742b963
Andrew Le <s3820098@rmit.edu.vn>
2022-02-14 17:16:37 +0700

    6. Quote Machine: fetch api and change quote box content

--- 1-random-quote-machine/index.js
+++ 1-random-quote-machine/index.js
@@ -1,1 +1,31 @@
-console.log("Hello Andrew world!");
+const textContainer = document.getElementById('text');
+const authorContainer = document.getElementById('author');
+const newQuoteButton = document.getElementById('new-quote');
+
+async function fetchQuotes() {
+  const response = await fetch(
+    'https://quotes15.p.rapidapi.com/quotes/random/',
+    {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'quotes15.p.rapidapi.com',
+        'x-rapidapi-key': 'a40a668e8amsh7b7a9299cb89da1p1a0699jsnf93fdd415d8e',
+      },
+    }
+  );
+  const data = await response.json();
+  const text = data.content;
+  const author = data.originator.name;
+  return { text, author };
+}
+
+function changeQuoteBoxContent() {
+  fetchQuotes().then((rsp) => {
+    textContainer.innerHTML = rsp.text;
+    authorContainer.innerHTML = '- ' + rsp.author;
+  });
+}
+
+changeQuoteBoxContent();
+
+newQuoteButton.addEventListener('click', () => changeQuoteBoxContent());
